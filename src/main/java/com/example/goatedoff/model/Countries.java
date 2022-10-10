833493a839fc570841c35b3af7717d7615db4702
mullera3 <amani.k.muller@gmail.comClotilda1_>
2022-10-10 15:01:19 -0400

    Added functionality to Countries.java

--- src/main/java/com/example/goatedoff/model/Countries.java
+++ src/main/java/com/example/goatedoff/model/Countries.java
@@ -4,4 +4,39 @@ public class Countries {
     private Integer countryCode;
     private String countryName;
     private String continentName;
+
+    public Countries(Integer countryCode, String countryName, String continentName) {
+        this.countryCode = countryCode;
+        this.countryName = countryName;
+        this.continentName = continentName;
+    }
+
+    public Countries()
+    {
+
+    }
+
+    public Integer getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(Integer countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getContinentName() {
+        return continentName;
+    }
+
+    public void setContinentName(String continentName) {
+        this.continentName = continentName;
+    }
 }
