976a8b2bc1864f0eb77a004a5a7696c4c35982c6
Shavandra <65180116+Shavandra@users.noreply.github.com>
2022-12-09 10:23:49 -0500

    Show Orders

--- backend/routes/ShowOrders.js
+++ backend/routes/ShowOrders.js
@@ -1,6 +1,6 @@
 function getDestinations() {
-    var uri = "http://services.odata.org/Northwind/Northwind.svc/Orders?orderby=OrderID&$select=OrderID,ShipName,ShipAddress,ShipCity,ShipPostalCode,ShipCountry&$format=json";
-    var xhr = new XMLHttpRequest();
+    const uri = "https://goatedoff-default-rtdb.firebaseio.com;
+    const xhr = new XMLHttpRequest();
     xhr.open("GET", uri, true);
     xhr.onload = function () {
         JSON.parse(xhr.responseText);
@@ -10,10 +10,10 @@ function getDestinations() {
 }
 
 function showDestinations(dest) {
-    var tableContent = "<tr class='orderTitle'><td>Order Id</td><td>Destination</td></tr>\n";
-    for (var i = 0; i < dest.length; ++i) {
-        var record = dest[i];
-        var addrs = record.ShipName + ", " + record.ShipAddress + ", " + record.ShipCity + ", " + record.ShipPostalCode + ", " + record.ShipCountry;
+    const tableContent = "<tr class='orderTitle'><td>Order Id</td><td>Destination</td></tr>\n";
+    for (const i = 0; i < dest.length; ++i) {
+        const record = dest[i];
+        const addrs = record.ShipName + ", " + record.ShipAddress + ", " + record.ShipCity + ", " + record.ShipPostalCode + ", " + record.ShipCountry;
         if (i & 1 == 1) { // odd row
             tableContent += "<tr class='orderOdd'>";
         }
