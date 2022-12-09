1fd198068c7620b01398a6228218a3b23238d735
Shavandra <65180116+Shavandra@users.noreply.github.com>
2022-12-09 12:00:58 -0500

    Show Orders

--- backend/routes/ShowOrders.js
+++ backend/routes/ShowOrders.js
@@ -1,26 +1,85 @@
-function getDestinations() {
-    const uri = "https://goatedoff-default-rtdb.firebaseio.com;
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", uri, true);
-    xhr.onload = function () {
-        JSON.parse(xhr.responseText);
-        showDestinations(books);
+import { Card,CardContent, Typography,CardActionArea,CardMedia, Button,IconButton } from '@material-ui/core';
+import { DeleteForeverRounded } from '@material-ui/icons';
+import React, { useContext } from 'react';
+import '../App.css';
+import { AuthContext } from '../context/AuthContext';
+
+const Cart = () => {
+    const {cartItems,setCartItems,setBadgeValue,badgeValue,totalPrice,setTotalPrice}=useContext(AuthContext);
+
+    const deleteCart=(id)=>{
+        const newItems= cartItems.filter((items)=>items.id!==id);
+        setCartItems(newItems);
+        setBadgeValue(prevValue=>--prevValue)
     }
-    xhr.send(null);
+
+    return (
+        <div className="cart-container">
+            <Typography className="font" variant="h4" style={{marginTop:"20px"}} align="center" gutterBottom>Your Cart</Typography>
+            <div className="cart-section">
+                <div className="cart-history">
+                    {cartItems!==null?cartItems.map((item,index)=>(
+                        <Card className="cart-history-flex" key={index}>
+                            <CardActionArea className="card-action">
+                                <CardMedia
+                                    component="img"
+                                    height="150"
+                                    alt={item.name}
+                                    image={item.img}
+                                />
+                            </CardActionArea>
+                            <CardContent className="flexing">
+                                <div className="flex1">
+                                    <Typography variant="body2" className="font">{item.name}</Typography>
+                                    <IconButton color="secondary" onClick={(e)=>deleteCart(item.id)}>
+                                        <DeleteForeverRounded fontSize="small"/>
+                                    </IconButton>
+                                </div>
+                                <div className="flex1">
+                                    <Typography  variant="body2" className="font">${item.price}</Typography>
+                                    <Typography variant="body2" className="font">QTY:{item.itemsLeft}</Typography>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    )):<Typography className="font" variant="h6" color="secondary" gutterBottom>Cart is empty</Typography>
+                    }
+                </div>
+                <div className="cart-summery">
+                    <Card>
+                        <CardContent>
+                            <Typography className="font" variant="h5" gutterBottom>ORDER SUMMERY</Typography>
+                            <div className="flex1">
+                                <Typography gutterBottom variant="body2" className="font" >{badgeValue}ITEMS</Typography>
+                                <Typography gutterBottom variant="body2" className="font" >${totalPrice}</Typography>
+                            </div>
+                            <div className="flex1">
+                                <Typography gutterBottom  variant="body2" className="font">DELEVERY</Typography>
+                                <Typography gutterBottom  variant="body2" className="font">FREE</Typography>
+                            </div>
+                            <div className="flex1">
+                                <Typography gutterBottom variant="body2" className="font">SALES TAX</Typography>
+                                <Typography gutterBottom  variant="body2" className="font"></Typography>
+                            </div>
+                            <div className="flex1">
+                                <Typography gutterBottom variant="body2" className="font">TOTAL</Typography>
+                                <Typography gutterBottom  variant="body2" className="font">${totalPrice}</Typography>
+                            </div>
+                            <Button
+                                style={{marginTop:"30px",width:"100%"}}
+                                size="small"
+                                className="font"
+                                variant="contained"
+                                color="secondary"
+                                onClick={(e)=>{setCartItems(null);setBadgeValue(0)}}
+                            >
+                                CHECKOUT
+                            </Button>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-function showDestinations(dest) {
-    const tableContent = "<tr class='orderTitle'><td>Order Id</td><td>Destination</td></tr>\n";
-    for (const i = 0; i < dest.length; ++i) {
-        const record = dest[i];
-        const addrs = record.ShipName + ", " + record.ShipAddress + ", " + record.ShipCity + ", " + record.ShipPostalCode + ", " + record.ShipCountry;
-        if (i & 1 == 1) { // odd row
-            tableContent += "<tr class='orderOdd'>";
-        }
-        else {
-            tableContent += "<tr class='orderEven'>";
-        }
-        tableContent += "<td>" + record.OrderID + "</td><td>" + addrs + "</td></tr>\n";
-    }
-    document.getElementById("showTab").innerHTML = tableContent;
-}
+export default Cart
